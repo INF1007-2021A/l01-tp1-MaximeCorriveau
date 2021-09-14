b2ed0eed89d8eca840e57e967daf1de080d482a8
MaximeCorriveau <maxime-2.corriveau@polymtl.ca>
2021-09-14 12:08:35 -0400

    Exercices 1,2,3,fait

--- exercice1.py
+++ exercice1.py
@@ -2,7 +2,16 @@ def fizzBuzz(n):
     # TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat
 
-    resultat =
+    resultat = n
+
+    if (n % 3) == 0 :
+        resultat = "fizz"
+    if (n % 5) == 0 :
+        resultat = "buzz"
+
+    if (n % 3) == 0 and (n % 5) == 0 :
+        resultat = "fizzbuzz"
+
     return resultat
 
 if __name__ == '__main__':
